0e1986af7724a333ee176a2f627ef016297e85e9
Nassir El abbassi <nassirelabbassi@mbp-de-nassir.home>
2025-04-15 19:40:53 +0200

    Initial Commit

---
+++ fortydays/fortydaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  fortydaysApp.swift
+//  fortydays
+//
+//  Created by Nassir El abbassi on 15/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct fortydaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fortydaysTests/fortydaysTests.swift
@@ -0,0 +1,17 @@
+//
+//  fortydaysTests.swift
+//  fortydaysTests
+//
+//  Created by Nassir El abbassi on 15/04/2025.
+//
+
+import Testing
+@testable import fortydays
+
+struct fortydaysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
