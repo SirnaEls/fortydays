6db831aaf1b442ebb435041490c6f7f20522ba1c
Nassir El abbassi <nassirelabbassi@mbp-de-nassir.home>
2025-04-16 20:02:45 +0200

    Connexion à Firebase, création de la vue Connexion et inscription

---
+++ fortydays/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  fortydays
+//
+//  Created by Nassir El abbassi on 16/04/2025.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
