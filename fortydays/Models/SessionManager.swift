8d2923fa8c7285eb3aa0d324bb082a59816a30ae
Nassir El abbassi <nassirelabbassi@mbp-de-nassir.home>
2025-04-29 19:40:51 +0200

    Ajout de plusieurs corrections :
    - Finalisation du sign in with apple avec le prénom qui est bien pris en compte dans la vue dashboard et profile
    - Ajout des couleurs pour le dark mode
    - Ajout du loader de connexion

---
+++ fortydays/Models/SessionManager.swift
@@ -0,0 +1,8 @@
+//
+//  SessionManager.swift
+//  fortydays
+//
+//  Created by Nassir El abbassi on 18/04/2025.
+//
+
+import Foundation
---
+++ fortydays/Views/AnimatedSplashView.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatedSplashView.swift
+//  fortydays
+//
+//  Created by Nassir El abbassi on 22/04/2025.
+//
+
+import SwiftUI
+
+struct AnimatedSplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimatedSplashView()
+}
